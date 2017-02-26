006d12c809b62d821f150fdc3c1886e799a06bbc
redgyuf <faramirex070@gmail.com>
2017-02-26 19:07:10 +0100

    init custom exceptions

---
+++ bolt/kivetel/NemLetezoAruKivetel.java
@@ -0,0 +1,7 @@
+package bolt.kivetel;
+
+public class NemLetezoAruKivetel extends Exception{
+	public NemLetezoAruKivetel(String message){
+		System.out.println(message);
+	}
+}
---
+++ bolt/kivetel/TulSokLevonasKivetel.java
@@ -0,0 +1,7 @@
+package bolt.kivetel;
+
+public class TulSokLevonasKivetel extends Exception{
+	public TulSokLevonasKivetel(String message){
+		System.out.println(message);
+	}
+}
