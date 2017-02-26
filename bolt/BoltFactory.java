52ab5a29d030589fac185605fddc21f8d77a4b77
redgyuf <faramirex070@gmail.com>
2017-02-26 19:24:38 +0100

    BoltFactory skel added

---
+++ bolt/BoltFactory.java
@@ -0,0 +1,33 @@
+package bolt;
+
+import java.util.Date;
+
+import bolt.aruk.Sajt;
+import bolt.aruk.Szappan;
+import bolt.aruk.Tej;
+
+public class BoltFactory {
+	public Tej ujTartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom){
+		return null;		
+	}
+	public Tej ujFeltartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom){
+		return null;		
+	}
+	public Tej ujFelzsirosTartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom){
+		return null;		
+	}
+	public Tej ujZsirosTartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom){
+		return null;		
+	}
+	public Tej ujFelzsirosFeltartosTej(Long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom){
+		return null;		
+	}
+	
+	public Sajt ujSajt(Long vonalKod, double suly, String gyarto, Date szavatossagiIdo, double zsirtartalom){
+		return null;
+	}
+	
+	public Szappan ujSzappan(long vonalKod, String gyarto, char mosohatas){
+		return null;
+	}
+}
