e70f124c03f75319ee08ca5d01cbc4fee1e4d524
redgyuf <faramirex070@gmail.com>
2017-02-26 17:45:45 +0100

    init, constructor added

---
+++ bolt/FeltartosTej.java
@@ -0,0 +1,12 @@
+package bolt;
+
+import java.util.Date;
+
+public class FeltartosTej extends Tej{
+
+	public FeltartosTej(long vonalKod, int urtaratalom, String gyarto, Date szavatossagiIdo, double zsirtartalom) {
+		super(vonalKod, urtaratalom, gyarto, szavatossagiIdo, zsirtartalom);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ bolt/TartosTej.java
@@ -0,0 +1,12 @@
+package bolt;
+
+import java.util.Date;
+
+public class TartosTej extends Tej{
+
+	public TartosTej(long vonalKod, int urtaratalom, String gyarto, Date szavatossagiIdo, double zsirtartalom) {
+		super(vonalKod, urtaratalom, gyarto, szavatossagiIdo, zsirtartalom);
+		// TODO Auto-generated constructor stub
+	}
+
+}
